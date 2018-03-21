f65407b1761b2d99820db79e2930182c470d15a8
一色優孝 <yff24517@nifty.com>
2018-03-21 18:58:09 +0900

    未収録曲がDBから削除されない問題を解決

--- get_similar_track.py
+++ get_similar_track.py
@@ -10,12 +10,20 @@ rolename =
 passwd = 
 
 
-def main():
+def dbconnect():
+    # DB初回接続(最近の聴取曲のリストを取得)
     conn = psycopg2.connect(database=dbname, host=host_name, port=port_number, user=rolename, password=passwd)
     cur = conn.cursor()
-    '''cur.execute("SELECT DISTINCT artist, track FROM ungot_similar_tracks ORDER BY artist DESC, track DESC")'''
-    cur.execute("SELECT DISTINCT to_artist, to_song FROM similar_track ORDER BY to_artist DESC, to_song ASC")
+    cur.execute("SELECT DISTINCT artist, track FROM ungot_similar_tracks ORDER BY artist DESC, track DESC")
+    '''cur.execute("SELECT DISTINCT to_artist, to_song FROM similar_track ORDER BY to_artist DESC, to_song ASC")'''
     item_list = cur.fetchall()
+    conn.close()
+    return item_list
+
+def data_search():
+    item_list = dbconnect()
+    conn = psycopg2.connect(database=dbname, host=host_name, port=port_number, user=rolename, password=passwd)
+    cur = conn.cursor()
     for it in item_list:
         artist = it[0]
         song = it[1]
@@ -44,23 +52,33 @@ def main():
                     data = json.loads(address_json.read())
                     similartracks = data["similartracks"]
                     track = similartracks["track"]
-                    for i in range(100):
-                        track_infomation = track[i]
-                        song_infomation = track_infomation["name"]
-                        match_index = track_infomation["match"]
-                        artist_infomation = track_infomation["artist"]
-                        artist_infomation = artist_infomation["name"]
-                        artist = urllib.parse.unquote(artist)
-                        song = urllib.parse.unquote(song)
-                        similar_track_data = (artist, song, artist_infomation, song_infomation, match_index)
-                        print(similar_track_data)
-                        cur.execute(
-                            "INSERT INTO similar_track(from_artist, from_song, to_artist, to_song, match_index)VALUES(%s, %s, %s, %s, %s)",
-                            similar_track_data)
+                    if track == []:
+                        cur.execute("INSERT INTO not_found_similar_track(artist, song)VALUES(%s, %s)",
+                                    got_similar_track_data)
                         conn.commit()
+                        print("該当トラックの情報がありませんでした")
                         cur.execute("DELETE from ungot_similar_tracks WHERE artist = %s AND track = %s",
                                     got_similar_track_data)
                         conn.commit()
+                        continue
+                    else:
+                        for i in range(len(track)):
+                            track_infomation = track[i]
+                            song_infomation = track_infomation["name"]
+                            match_index = track_infomation["match"]
+                            artist_infomation = track_infomation["artist"]
+                            artist_infomation = artist_infomation["name"]
+                            artist = urllib.parse.unquote(artist)
+                            song = urllib.parse.unquote(song)
+                            similar_track_data = (artist, song, artist_infomation, song_infomation, match_index)
+                            print(similar_track_data)
+                            cur.execute(
+                            "INSERT INTO similar_track(from_artist, from_song, to_artist, to_song, match_index)VALUES(%s, %s, %s, %s, %s)",
+                            similar_track_data)
+                            conn.commit()
+                            cur.execute("DELETE from ungot_similar_tracks WHERE artist = %s AND track = %s",
+                                    got_similar_track_data)
+                            conn.commit()
                 except IndexError:
                     artist = urllib.parse.unquote(artist)
                     song = urllib.parse.unquote(song)
@@ -104,4 +122,4 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    data_search()
